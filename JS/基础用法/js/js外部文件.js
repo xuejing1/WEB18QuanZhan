40847f629c830121794149ebe264a6f3e1b88cd6
unknown <1776202826@qq.com>
2018-04-27 13:09:37 +0800

    del

--- JS/基础用法/js/js外部文件.js
+++
@@ -1,4 +0,0 @@
-function toRed(){
-	// 需要获取元素 设置背景颜色
-	document.getElementById('box').style.backgroundColor = 'red';
-}
--- JS/基础用法/js/js外部文件2.js
+++
@@ -1,6 +0,0 @@
-function toRed(){
-	// 设置样式
-	document.getElementById('box').style.background='red';
-}
-// 执行事件
-document.getElementById('box').onclick=toRed;
