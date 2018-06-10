d25eeebb917c8b81dd5f46192d187c070183b331
鲤鱼IT <1776202826@qq.comomm>
2018-06-10 17:39:39 +0800

    jQuery案例

---
+++ 05jQuery框架/index.js
@@ -0,0 +1,12 @@
+;(function(w){
+	var transitionEventName={
+		transition:'transitionend',
+		MozTransition:'transitionend',
+		WebkitTransition:'webkitTransitionEnd',
+		OTransition:'oTransitionend'
+	}
+	var transition={
+		end:'',
+		isSupport:false;
+	}
+})(window);
