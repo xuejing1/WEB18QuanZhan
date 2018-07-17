c7debd4cad6d06259819a9df07f70ffd52b100f5
鲤鱼IT <1776202826@qq.comomm>
2018-07-17 18:05:21 +0800

    京东案例

--- 07CSS进阶/案例/js/index.js
+++ 07CSS进阶/案例/js/index.js
@@ -1,17 +1,14 @@
 (function(win,doc){
-	//1.获取根元素
-	var oRoot = doc.getElementsByTagName('html')[0];
-	
+	var oRoot=doc.getElementsByTagName('html')[0];
 	function refreshFontSize(){
-		//2.获取设备的宽
-		var iWidth = doc.body.clientWidth || doc.documentElement.clientWidth;
-		//3.根据设备的宽计算出根元素的fontSize
-		var iFontSize = iWidth / 10;
-		oRoot.style.fontSize = iFontSize + 'px';			
+		var iWidth=doc.body.clientWidth || doc.documentElement.clientWidth;
+		var iFontSize=iWidth/10;
+		oRoot.style.fontSize=iFontSize+'px';			
 	}
-
-
 	win.addEventListener('resize',refreshFontSize,false);
 	doc.addEventListener('DOMContentLoaded',refreshFontSize,false);
-
 })(window,document);
+
+function selected(){
+	var oSign=document.getElementById('asign');
+}
