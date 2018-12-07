5fc06ead0f844f5e40ee7ccdf69a61bee24e2059
unknown <1776202826@qq.com>
2018-12-07 14:21:01 +0800

    js基础

--- 03-JavaScript/js/009-js引用.js
+++ 03-JavaScript/js/009-js引用.js
@@ -1,4 +1,3 @@
-
 var oBox=document.getElementById('box');
 oBox.onclick=function(){
 	oBox.style.background='yellow';
---
+++ 03-JavaScript/js/010.js
@@ -0,0 +1,7 @@
+
+window.onload=function(){
+	var oBox=document.getElementById('box');
+		oBox.onclick=function(){
+		oBox.style.background='yellow';
+	}
+}
